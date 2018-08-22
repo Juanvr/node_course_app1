6f82cfdc6de7b3fc4a3168fd3a7ec918eb4be9c0
Juancho <jvelazquezromera@gmail.com>
2018-08-22 19:54:31 +0200

    Before environment variables

--- index.js
+++ index.js
@@ -12,49 +12,97 @@ var StringDecoder = require('string_decoder').StringDecoder;
 
 var server = http.createServer(function(req, res){
 
-// Get the URL and parse it
-var parsedUrl = url.parse(req.url,true);
+  // Get the URL and parse it
+  var parsedUrl = url.parse(req.url,true);
 
-// Get the path
-var path = parsedUrl.pathname;
-var trimmedPath = path.replace(/^\/+|\/+$/g, '');
+  // Get the path
+  var path = parsedUrl.pathname;
+  var trimmedPath = path.replace(/^\/+|\/+$/g, '');
 
-// Get the query string as an object
-var queryStringObject = parsedUrl.query;
+  // Get the query string as an object
+  var queryStringObject = parsedUrl.query;
 
-// Get the HTTP method
-var method =  req.method.toLowerCase();
+  // Get the HTTP method
+  var method =  req.method.toLowerCase();
 
-// Get the headers as an object
-var headers = req.headers;
+  // Get the headers as an object
+  var headers = req.headers;
 
-// Get the payload, if any
-var decoder = new StringDecoder('utf-8');
-var buffer = '';
-req.on('data', function(data){
-  buffer += decoder.write(data);
+  // Get the payload, if any
+  var decoder = new StringDecoder('utf-8');
+  var buffer = '';
+  req.on('data', function(data){
+    buffer += decoder.write(data);
+  });
+
+  req.on('end', function(){
+    buffer += decoder.end();
+
+    // Choose the handler this request should go to. If one is not found, use not found handler
+    var chosenHandler = typeof(router[trimmedPath]) !== 'undefined' ? router[trimmedPath] : handlers.notFound;
+
+    // Construct the data object to send to the handler
+    var data = {
+      'trimmedPath' : trimmedPath,
+      'queryStringObject' : queryStringObject,
+      'method' : method,
+      'headers' : headers,
+      'payload' : buffer
+    };
+
+    // Route the request to the handler specified in the router
+    chosenHandler(data, function (statusCode, payload){
+      // Use the status code called back by the handler, or default to 200
+      statusCode = typeof(statusCode) == 'number' ? statusCode : 200;
+      // Use the payload called back by the handler, or default to an empty object
+      payload = typeof(payload) == 'object' ? payload : {};
+
+      // Convert the payload to a string
+      var payloadString = JSON.stringify(payload);
+
+      // Return the response
+      res.setHeader('Content-Type', 'application/json');
+      res.writeHead(statusCode);
+      res.end(payloadString);
+
+      console.log('Returning this response: ', statusCode, payloadString);
+    });
+
+
+    // Send the response
+    //res.end('Hello World\n');
+
+    // Log the request path
+    //console.log('Request received on path: ' + trimmedPath + ' with method '+ method
+    //+ ' and with these query string parameters: ', queryStringObject);
+
+    //console.log('Request received with these headers: ', headers);
+    //console.log('Request received with this payload: ', buffer);
+  });
+
 });
-
-req.on('end', function(){
-  buffer += decoder.end();
-
-  // Send the response
-  res.end('Hello World\n');
-
-  // Log the request path
-  console.log('Request received on path: ' + trimmedPath + ' with method '+ method
-  + ' and with these query string parameters: ', queryStringObject);
-
-  console.log('Request received with these headers: ', headers);
-  console.log('Request received with this payload: ', buffer);
-});
-
-})
-
-
-
 
 // Start the server, and have it listen on port 3000
 server.listen(3000, function(){
   console.log("The server is listening on port 3000 now");
-})
+});
+
+// Define the handlers
+var handlers = {};
+
+// Sample handler
+handlers.sample = function (data, callback){
+  // Callback a http status code, and a payload object
+  callback(406, {'name' : 'sample handler'});
+};
+
+// Not found handler
+handlers.notFound = function (data, callback){
+  callback(404);
+
+};
+
+// Define a request router
+var router = {
+  'sample' : handlers.sample
+};
